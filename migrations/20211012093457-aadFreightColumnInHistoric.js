f598d261d089a50d375631afa0c024e4aead6b16
Chirac Ilunga <tshitengailunga@icloud.com>
2021-10-14 12:34:07 +0100

    first commit

---
+++ migrations/20211012093457-aadFreightColumnInHistoric.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      "Historics", // table name
+      "freight", // new field name
+      {
+        type: Sequelize.STRING,
+        defaultValue: "Payé",
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("Historics", "freight");
+  },
+};
